367219875d6b875e560e5559a647496641bf347c
Gaboemi-Duoc <173064947+Gaboemi-Duoc@users.noreply.github.com>
2026-09-08 09:50:41 -0300

    Initial Proyect

---
+++ src/main/java/alumno/gaboemi/cloudevaluacion/CloudEvaluacionApplication.java
@@ -0,0 +1,13 @@
+package alumno.gaboemi.cloudevaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEvaluacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEvaluacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alumno/gaboemi/cloudevaluacion/CloudEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package alumno.gaboemi.cloudevaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEvaluacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
