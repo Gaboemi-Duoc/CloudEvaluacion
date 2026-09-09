da7f8b986c8f0ccc0408a2d89fd1f01ee1d90e17
Gaboemi-Duoc <173064947+Gaboemi-Duoc@users.noreply.github.com>
2026-09-09 10:55:26 -0300

    feat: Implemented Incognito Security

--- src/main/java/alumno/gaboemi/cloudevaluacion/controller/CourseController.java
+++ src/main/java/alumno/gaboemi/cloudevaluacion/controller/CourseController.java
@@ -1,5 +1,19 @@
 package alumno.gaboemi.cloudevaluacion.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("api/course")
 public class CourseController {
 
+    @GetMapping
+    public String getMethodName() {
+        return new String();
+    }
+    
+    
 }
---
+++ src/main/java/alumno/gaboemi/cloudevaluacion/service/CourseService.java
@@ -0,0 +1,8 @@
+package alumno.gaboemi.cloudevaluacion.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+    
+}
